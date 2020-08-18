2983754c99e10051f7405c9dd4e86041ac361845
Unnsteinn Garðarsson <unnsteinng@gmail.com>
2020-08-18 09:14:18 +0000

    adds forgotten export to logger

--- server/shared/logging/logger.js
+++ server/shared/logging/logger.js
@@ -4,3 +4,5 @@ const logger = require('pino')({
   level: process.env.NODE_ENV === 'test' ? 'silent' : process.env.LOG_LEVEL || 'debug',
   name: 'mr-gadget-server',
 });
+
+module.exports = logger;
