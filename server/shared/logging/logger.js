d1c05c32ad47953425870a0e539de999b4bb5575
Unnsteinn Garðarsson <unnsteinng@gmail.com>
2020-08-18 09:13:09 +0000

    initializes a central pino logger

---
+++ server/shared/logging/logger.js
@@ -0,0 +1,6 @@
+require('dotenv').config();
+
+const logger = require('pino')({
+  level: process.env.NODE_ENV === 'test' ? 'silent' : process.env.LOG_LEVEL || 'debug',
+  name: 'mr-gadget-server',
+});
