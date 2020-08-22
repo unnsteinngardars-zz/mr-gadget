530a322ec3e325c587ebd2a2c8997ba28cdff61f
Unnsteinn Garðarsson <unnsteinng@gmail.com>
2020-08-22 09:17:56 +0000

    adds eslint:recommended and airbnb to eslintrc in client

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -4,8 +4,9 @@ module.exports = {
     es6: true,
   },
   extends: [
-    'plugin:react/recommended',
     'airbnb',
+    'eslint:recommended',
+    'plugin:react/recommended',
   ],
   globals: {
     Atomics: 'readonly',
